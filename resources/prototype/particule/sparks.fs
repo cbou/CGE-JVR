52884a6fa0ea8c888d1e8e117b8560e74b064995
charles bourasseau <charles.bourasseau@gmail.com>
2011-06-26 11:47:53 +0200

    integrate particule system to game

--- resources/prototype/particule/sparks.fs
+++ resources/prototype/particule/sparks.fs
@@ -2,5 +2,6 @@ varying float partRadiusG;
 
 void main (void)
 {
-  gl_FragColor = vec4(1, 1, 1, partRadiusG - 1.2);
+  //gl_FragColor = vec4(1, 0, 0, partRadiusG - 1.2);
+  gl_FragColor = vec4(1, 0, 0, 1);
 }
